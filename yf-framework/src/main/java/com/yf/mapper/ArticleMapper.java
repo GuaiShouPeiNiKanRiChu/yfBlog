093fa610ab971238c87e3533f1f18a112a59220f
biyunfei3 <biyunfei3@jd.com>
2024-11-07 13:08:10 +0800

    个人博客

---
+++ yf-framework/src/main/java/com/yf/mapper/ArticleMapper.java
@@ -0,0 +1,11 @@
+package com.yf.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yf.domain.entity.Article;
+
+/**
+ * @author: biyunfei3
+ * @create: 2024-11-07 11:32
+ **/
+public interface ArticleMapper extends BaseMapper<Article> {
+}
