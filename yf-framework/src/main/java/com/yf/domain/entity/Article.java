2994b9bf1b69181a29f1ea669f770e31e374ea86
biyunfei3 <biyunfei3@jd.com>
2024-11-07 11:28:50 +0800

    个人博客

---
+++ yf-framework/src/main/java/com/yf/domain/entity/Article.java
@@ -0,0 +1,42 @@
+package com.yf.domain.entity;
+
+import java.util.Date;
+
+/**
+ * @author: biyunfei3
+ * @create: 2024-11-07 11:21
+ **/
+public class Article {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String type;
+
+    private String  summary;
+
+    private String  categoryId;
+
+    private String thumbnail;
+
+    private String isTop;
+
+    private String status;
+
+    private Long viewCount;
+
+    private String isComment;
+
+    private Long createBy;
+
+    private Date createTime;
+
+    private Long updateBy;
+
+    private Date updateTime;
+
+    private int delFlag;
+}
