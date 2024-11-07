b3736821072fda3a0363c9cfcf7862c57ccda47f
biyunfei3 <biyunfei3@jd.com>
2024-11-07 09:23:54 +0800

    个人博客

--- yf-blog/src/main/java/yf/YFBlogApplication.java
+++ yf-blog/src/main/java/yf/YFBlogApplication.java
@@ -1,14 +1,17 @@
 package yf;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author: biyunfei3
  * @create: 2024-11-01 15:20
  **/
-
+@SpringBootApplication
 public class YFBlogApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(YFBlogApplication.class, args);
     }
 
 }
